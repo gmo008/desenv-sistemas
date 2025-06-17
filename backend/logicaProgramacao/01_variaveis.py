c38cec2b6cf95ad60a26815187f73e31080caed6
Geovana Muniz <geovanamunizdeoliveira424@gmail.com>
2025-06-17 09:47:30 -0300

    Trabalhando com variáveis

---
+++ backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,36 @@
+nome = "Geovana"
+idade = 17
+altura = 1.70
+tem_carteira = False 
+
+print("Nome:", nome)
+print("idade:", idade)
+print("Altura:", altura)
+print("Tem carteira?", tem_carteira)
+
+
+roupa = "Blusa"
+animal = "Coelho"
+
+print("Roupa:", roupa)
+print("Animal:", animal)
+ 
+mes = 8
+ano = 2008
+
+print("Mes:", mes)
+print("Ano:", ano)
+
+peso = 51,10
+temperatura = 15,9 
+
+print("Peso:", peso)
+print("Temperatura:", temperatura)
+
+tem_carro = False
+tem_unha = True 
+
+print("Tem carro?", tem_carro)
+print("Tem unha?", tem_unha)
+
+ 
