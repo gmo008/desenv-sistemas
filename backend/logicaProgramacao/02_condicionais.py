efec92bd7bbda3e1e00f3a9a57a2e206947fd83a
Geovana Muniz <geovanamunizdeoliveira424@gmail.com>
2025-06-23 09:06:00 -0300

    Trabalhando com Condições

---
+++ backend/logicaProgramacao/02_condicionais.py
@@ -0,0 +1,12 @@
+nota = int(input("Digite sua nota: "))
+
+if(nota >= 80):
+    print("Parabens.")
+
+elif(nota < 80 and nota >60):
+    print("Pode melhorar")
+
+elif(nota >60):
+    print("Recuperação")
+
+
