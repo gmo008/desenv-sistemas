242987718437a9081f90f4d8be774ab08e5fb676
geovana <geovanamunizdeoliveira424@gmail.com>
2025-07-22 09:54:15 -0300

    exercicio revisao

---
+++ backend/logicaProgramacao/05_exercicio_revisão.py
@@ -0,0 +1,15 @@
+valor1 =int(input("digite seu valor:"))
+valor2 =int(input("digite seu valor:"))
+ 
+soma = (valor1 + valor2)
+print("sua soma foi:", soma)
+
+subtracao = (valor1 - valor2)
+print("sua subtracao foi:", subtracao)
+
+multiplicacao = (valor1 * valor2)
+print("sua multiplicacao foi:", multiplicacao)
+
+divisao = (valor2 / valor2)
+print("sua divisao foi:", divisao)
+
