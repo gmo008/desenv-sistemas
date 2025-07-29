469184976d5c791ec69d10610e80a57544249cb9
geovana <geovanamunizdeoliveira424@gmail.com>
2025-07-29 09:35:31 -0300

    exercicio laço 02

---
+++ backend/logicaProgramacao/04_exerciciolaço.py
@@ -0,0 +1,5 @@
+pedir nome e senha ao usuario 
+mostrar "bem vindo" quando acertar a senha e o nome.
+apos, pedir o salario do usuario 
+mostrar salario anual
+se o salario anual for maior que 100 mil mostrar mensagem "rico"
