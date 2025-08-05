085e682c2f0d23f5da759dfa7fcb5b614a057255
geovana <geovanamunizdeoliveira4242gmail.com>
2025-08-05 09:45:02 -0300

    exercicio função

---
+++ backend/logicaProgramacao/exercicio_função01.py
@@ -0,0 +1,15 @@
+#Crie uma função que faça a média de 3 valores
+
+def calculomedia(valor1, valor2, valor3):
+    calculomedia = (valor1 + valor2 + valor3) / 3
+    print("sua media foi")
+
+#Crie uma função que calcule o imposto anual do seu salário - 22%
+
+def impostoanual(salario_mensal):
+    salario_anual = salario_mensal * 12
+    imposto = salario_anual * 0.15
+    print("imposto mensal", impostoanual)
+
+#Crie uma função que valide se a senha está correta
+
