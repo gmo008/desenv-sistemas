c4d66527ab4fc3df4c7071cb698803fe583ef2b8
geovana <geovanamunizdeoliveira424@gmail.com>
2025-08-19 09:41:27 -0300

    Exercicio lista

---
+++ backend/logicaProgramacao/01_lista.py
@@ -0,0 +1,17 @@
+#Crie uma lista de tarefas
+#Adicione 5 tarefas na lista
+#Adicione uma tarefa na posição 2 da lista
+#Remova a tarefa "lavar louça" da lista 
+#Remover a tarefa da posição 1 da lista
+
+tarefas = [
+    "Lavar louça"
+    "Fazer compra"
+    "Estudar"
+    "Lavar biz"
+    "Dar banho no cachorro"
+
+]
+
+tarefas.insert(2, "Trabalhar")
+tarefas.remove("Lavar louça")
---
+++ backend/logicaProgramacao/02_lista.py
@@ -0,0 +1,17 @@
+#01 Crie uma lista de 10 alunos mostre "Olá" + nome de cada aluno
+#02 Crie uma lista de 10 números mostre a soma de todosos números da lista
+#03 Crie uma lista de 20 números diga quantos números pares e ímpares tem na lista
+
+#01
+alunos = ["Geovana", "Bianca", "Beatriz", "Paula", "Julia", "Adriana", "Davi", "João Gabril", "Paulo", "Emily"]
+
+for aluno in aluno:
+    print("Olá" + aluno)
+ 
+ 
+#02
+numeros = []
+numeros.append= (1,2,3,4,5,6,7,8,9,10)
+
+for numero in numeros:
+soma += numeros
