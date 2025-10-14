5b9446ab28485dff308acacf59f0cdebcbd911b9
geovana <geovanamunizdeoliveira424@gmail.com>
2025-10-14 09:43:40 -0300

    criando projeto django

--- backend/logicaProgramacao/exercicio_completo_json.py
+++ backend/logicaProgramacao/exercicio_completo_json.py
@@ -47,3 +47,79 @@ with open("petshop.json", "w")as arquivo:
 #-----modificar produto-----
 id_produto_modificar = int (input("digite o id para modificar"))
 novo_nome = int(input("digite o novo nome"))
+nova_raça = input("digite a nova raca: ")
+nova_idade = input("digite a nova idade: ")
+novo_sexo = input("digite o novo sexo: ")
+novo_nome_dono = input("digite o novo nome do dono: ")
+novo_telefone_dono = input("digite o novo telefone do dono: ")
+
+for pet in pets:
+    if pets['id'] == id_pets_modificador:
+        pet['nome'] = novo_nome
+        pet['raca'] = nova_raca
+        pet['idade'] = nova_idade
+        pet['sexo'] = novo_sexo
+        pet_encontrado = True
+        break;
+
+        if not pet_encontrado:
+    print("o pet com o id informado não foi encontrado")
+
+else:
+    with open ("petshop.json", "w") as arquivo:
+        json.dump(inventario, arquivo, indent = 4)
+    print("o arquivo foi alterado com sucesso!!")
+
+
+#listar pet do arquivo json
+    
+try:
+    with open ("petshop.json",'r') as arquivo:
+        inventario= json.load(arquivo)
+
+    if not inventario:
+        print("O arquivo está vazio!")
+
+    else:
+        print("----- Lista de pets cadastrados -----")
+        for pet in inventario:
+            print(f"\n-- Pet {pet.get('id')} --")
+            print(f"Nome: {pet.get('nome', 'n/a')}")
+            print(f"Raça: {pet.get('raca', 'n/a')}")
+            print(f"Sexo: {pet.get('sexo', 'n/a')}")
+            print(f"Idade: {pet.get('idade', 0)} anos")
+            print(f"Nome do dono: {pet.get('nome_dono', 'n/a')}")
+            print(f"Telefone do dono: {pet.get('telefone_dono', 'n/a')}")
+
+except FileNotFoundError:
+    print("arquivo não encontrado")
+
+
+
+#excluir pet no json
+
+try:
+    with open("petshop.json", "r") as arquivo:
+        inventario = json.load(arquivo)
+
+except FileNotFoundError:
+    print("Arquivo não encontrado")
+
+
+novo_inventario = []
+pet_excluido = False
+
+id_pet_excluir = int(input("digite o  id do pet para excluir: "))
+
+for pet in inventario:
+    if pet["id"] != id_pet_excluir:
+    #se o id for diferente adicionamos  a nova lista 
+        novo_inventario.append(pet)
+    else:
+        print("pet removido com sucesso!!")
+        pet_excluido = True
+
+else:
+    with open('petshop.json', 'w') as arquivo:
+        json.dump(novo_inventario, arquivo, indent=4)
+        print('o arquivo foi atualizado, pet removido')
