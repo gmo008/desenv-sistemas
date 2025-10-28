ab94a09f1057392bd77939c10808a5e4c90b5384
geovana <geovanamunizdeoliveira424@gmail.com>
2025-10-28 09:00:39 -0300

    alterando tarefa

--- gerenciador_tarefas/config/tarefas/views.py
+++ gerenciador_tarefas/config/tarefas/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
-from models import Tarefa
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Tarefa
 from django.http import HttpResponse
 
 def listar_tarefas(request):
     tarefas_salvas = Tarefa.objects.all()
     contexto = {
-             "minhas_tarefas": tarefas salvas
+             "minhas_tarefas": tarefas_salvas
 }
              
     return render(request, 'tarefas/lista.html',contexto)
+
+def detalhe_tarefa(request, tarefa_id):
+    tarefa = get_object_or_404(Tarefa, pk=tarefa_id)
+    return render(request, 'tarefas/detalhe.html', {'tarefa': tarefa})
+
+def adicionar_tarefa (request):
+    if request.method == 'POST':
+        Titulo = request.POST.Get('titulo')
+        descricao = request.POST.Get('descricao')
+        Tarefa.objects.create(titulo=titulo,descricao=descricao)
+        return redirect('lista_tarefas')
+
+    return render (request, 'tarefas/form_tarefa.html')
+    
+def alterar_tarefa (request, tarefa_id):
+    tarefa= get_object_or_404(Tarefa, pk= tarefa_id)
+    if request.method == 'POST':
+        tarefa.titulo = request.POST.get('titulo')
+        tarefa.descricao = request.POST.get('descricao')
+        tarefa.concluida = request.POST.get('concluida') == 'on'
+        tarefa.save()#salva as alterações no objeto existente
+        return redirect ('lista_tarefas')
+    return render(request, 'tarefas/form_tarefa.html', {'tarefa': tarefa})
